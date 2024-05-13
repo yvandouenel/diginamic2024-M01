f2ca180b96e93706251a39e6818ce8da45f43ebc
yvandouenel <y.douenel@coopernet.fr>
2024-05-13 15:01:57 +0200

    exo todolist corrigé.

--- js/exos/06exoTodoList.js
+++ js/exos/06exoTodoList.js
@@ -2,30 +2,25 @@ import { createMarkup } from "./utils/tools.js";
 import FormAddTask from "./components/FormAddTask.js";
 import Task from "./components/Task.js";
 
-// Création du tableau de tâches
-let tasksList = [];
 
+class TodoList {
+  constructor() {
+    this.tasksList = [];
+    this.form = new FormAddTask(this.tasksList, this.handleUpTasksList);
+    this.wrapperTasks = createMarkup("section", document.body);
 
-// création du formulaire
-const form = new FormAddTask(tasksList, handleUpTasksList);
+    this.handleUpTasksList();
+  }
+  handleUpTasksList = () => {
+    console.log(`this.tasksList dans handleUpTasksList`, this.tasksList);
+    // Suppression de tous les enfants de wrapperTasks
+    this.wrapperTasks.innerHTML = "";
 
-// Création du conteneur de la liste des tâches
-const wrapperTasks = createMarkup("section", document.body);
-
-/**
- * Fonction appelée lors d'un changement sur le tableau des tâches
- * 
- * 
- */
-function handleUpTasksList() {
-  console.log(`tasksList dans handleUpTasksList`, tasksList);
-
-  // Suppression de tous les enfants de wrapperTasks
-  wrapperTasks.innerHTML = "";
-
-  // Affichage des tasks en utilisant le composant Task.js
-  tasksList.sort((a, b) => a.done - b.done).forEach(task => {
-    new Task(task.label, task.done, task.id, wrapperTasks, tasksList, handleUpTasksList);
-  })
+    // Affichage des tasks en utilisant le composant Task.js
+    this.tasksList.sort((a, b) => a.done - b.done).forEach(task => {
+      new Task(task.label, task.done, task.id, this.wrapperTasks, this.tasksList, this.handleUpTasksList);
+    })
+  }
 }
+new TodoList();
 
