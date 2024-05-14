7691535ab63c7837d8a6c5a5c9efa83811fcf0c4
yvandouenel <y.douenel@coopernet.fr>
2024-05-14 15:38:20 +0200

    suite exo todoList avec Delete.

--- js/exos/components/Task.js
+++ js/exos/components/Task.js
@@ -1,4 +1,5 @@
 import { createMarkup } from "../utils/tools.js";
+import FetchTasks from "../services/FetchTasks.js";
 export default class Task {
   constructor(label, done, id, wrapperTasks, tasksList, handleUpTasksList) {
     this.label = label;
@@ -7,20 +8,54 @@ export default class Task {
     this.wrapperTasks = wrapperTasks;
     this.tasksList = tasksList;
     this.handleUpTasksList = handleUpTasksList;
+    this.sectionError = createMarkup("section", document.body, "", { id: "error-section" });
 
     this.domElts = this.render();
     this.handleEvents()
   }
+  /**
+ * Affiche temporairement une erreur 
+ * @param {String} error 
+ */
+  showError(error) {
+    const errorMsg = createMarkup("p", this.sectionError, error, {
+      "class": "text-white bg-secondary mt-3 float-end w-25 p-3 rounded"
+    });
+    const animation = [
+      { fontSize: "100%" },
+      { fontSize: "110%" },
+      { fontSize: "100%", offset: 0.3 },
+      { fontSize: "100%", offset: 0.9 },
+      { opacity: "0" },
+    ];
+
+    const animationTiming = {
+      duration: 10000,
+      iterations: 1,
+    };
+    errorMsg.animate(animation, animationTiming);
+    setTimeout(() => {
+      this.sectionError.innerHTML = ""
+    }, 10000)
+  }
   handleEvents() {
-    this.domElts.btnDelete.addEventListener("click", () => {
+    this.domElts.btnDelete.addEventListener("click", async () => {
       console.log(`Suppression de la tâche : `, this.id);
-      // Suppression de la tâche concernée
+      // Suppression locale de la tâche concernée
       this.tasksList.forEach((task, index) => {
         if (task.id == this.id) {
           this.tasksList.splice(index, 1);
         }
       });
-      this.handleUpTasksList()
+      this.handleUpTasksList();
+      // Suppression distante via json-server de la tâche concernée
+      try {
+        const deletedTask = await FetchTasks.deleteTask(this.id);
+        console.log(`Element supprimé : `, deletedTask);
+      } catch (error) {
+        console.error("Erreur attrapée dans handleEvents de Task");
+        this.showError("Erreur lors de la suppression d'une tâche : " + error);
+      }
     })
     this.domElts.btnValidate.addEventListener("click", () => {
       console.log(`Modification de la tâche : `, this.id);
--- js/exos/services/FetchTasks.js
+++ js/exos/services/FetchTasks.js
@@ -6,4 +6,15 @@ export default class FetchTasks {
       .then(response => response.json())
       .then(data => data)
   }
+  static async deleteTask(id) {
+    return fetch(FetchTasks.url + `qsdf/${id}`,
+      {
+        method: "DELETE",
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log(`data dans deleteTask`, data);
+        return data;
+      })
+  }
 }
