ac2ba522e516e5111b1c71497f7edae58a8a5fc4
yvandouenel <y.douenel@coopernet.fr>
2024-05-14 12:59:03 +0200

    correction exo univ.

--- js/exos/07ExoUniv.js
+++ js/exos/07ExoUniv.js
@@ -2,10 +2,31 @@ import FetchUniv from "./services/FetchUniv.js";
 import { createMarkup } from "./utils/tools.js";
 import Univ from "./components/Univ.js";
 
+let univs = [];
+
 // Récupération d'une référence au formulaire de choix du pays
 const formCountries = document.getElementById("form-countries");
 
 const sectionError = createMarkup("section", document.body, "", { "class": "container" });
+
+
+// Eléments du dom pour filtrer
+const sectionFilter = createMarkup("section", document.body, "", { "class": "container mt-2" });
+const labelFilter = createMarkup("label", sectionFilter, "Filtrer les résultats : ", { "class": "me-2" });
+const inputFilter = createMarkup("input", sectionFilter, "", { "type": "text", "class": "ml-2" });
+sectionFilter.hidden = true;
+
+// gestion de l'événement input sur l'input de filtrage
+inputFilter.addEventListener("input", function (e) {
+  console.log(`input`);
+  const inputValue = e.target.value;
+  if (inputValue.length > 2) {
+    generateUnivs(univs.filter((univ) => univ.name.toLowerCase().includes(inputValue.toLowerCase())));
+  }
+  else {
+    generateUnivs(univs);
+  }
+})
 
 const sectionUnivs = createMarkup("section", document.body, "", { "class": "container" });
 
@@ -22,40 +43,50 @@ formCountries.addEventListener("submit", async (event) => {
 
   // Appel du service pour récupérer le retour de la requête http via fetch
   try {
-    const univs = await FetchUniv.loadUnivs(countrie);
+    univs = await FetchUniv.loadUnivs(countrie);
+    sectionFilter.hidden = false;
+    inputFilter.value = "";
     console.log(`univs`, univs);
 
-    // Suppression des éventuels éléments du DOM enfants de sectionUnivs
-    sectionUnivs.innerHTML = "";
+    generateUnivs(univs);
 
-    // génération des éléments du dom via la class Univ
-    const wrapperUniv = createMarkup("div", sectionUnivs, "", { "class": "row" });
-    univs.forEach(univ => {
-      new Univ(univ.name, univ?.domains[0], wrapperUniv);
-    })
   } catch (error) {
     console.error("Erreur attrapée : ", error);
     showError(`Une erreur est survenue lors de la récupération des données : ${error}`);
   }
 })
+
+function generateUnivs(univs) {
+  // Suppression des éventuels éléments du DOM enfants de sectionUnivs
+  sectionUnivs.innerHTML = "";
+
+  const nbUnivs = createMarkup("p", sectionUnivs, `${univs.length} résultats`);
+  // génération des éléments du dom via la class Univ
+  const wrapperUniv = createMarkup("div", sectionUnivs, "", { "class": "row" });
+  univs.forEach(univ => {
+    new Univ(univ.name, univ?.domains[0], wrapperUniv);
+  })
+}
 /**
  * Affiche temporairement une erreur 
  * @param {String} error 
  */
 function showError(error) {
-  const errorMsg = createMarkup("p", sectionError, error, { "class": "text-danger" });
+  const errorMsg = createMarkup("p", sectionError, error, { "class": "text-white bg-secondary mt-3 float-end w-25 p-3 rounded" });
   const animation = [
     { fontSize: "100%" },
-    { fontSize: "300%" },
-    { fontSize: "100%" },
+    { fontSize: "110%" },
+    { fontSize: "100%", offset: 0.3 },
+    { fontSize: "100%", offset: 0.9 },
+    { opacity: "0" },
   ];
 
   const animationTiming = {
-    duration: 2000,
+    duration: 10000,
     iterations: 1,
   };
   errorMsg.animate(animation, animationTiming);
   setTimeout(() => {
     sectionError.innerHTML = ""
-  }, 3000)
+  }, 10000)
 }
