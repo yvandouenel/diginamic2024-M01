7e92a4ccbd6d4f003a5acf59573add69df74ec1a
yvandouenel <y.douenel@coopernet.fr>
2024-05-31 12:43:26 +0200

    Premier test observable froid.

--- webpack/03testRxJS/src/index.ts
+++ webpack/03testRxJS/src/index.ts
@@ -1,5 +1,22 @@
+import { Observable } from "rxjs";
 console.log(`HelloWorld`);
 
-// Création d'un observable
-const obs1 = new Observable();
-console.log(`obs1`,obs1);
+// Création d'un observable froid
+const obs1$ = new Observable((observer) => {
+  // Notification next synchrone
+  observer.next(1);
+  setTimeout(() => {
+    observer.next(2);
+  }, 2000);
+});
+console.log(`obs1`, obs1$);
+
+// Souscription à l'observable
+const subscription1 = obs1$.subscribe({
+  next: (data) => console.log(`data dans observer 1`, data),
+});
+setTimeout(() => {
+  const subscription2 = obs1$.subscribe({
+    next: (data) => console.log(`data dans observer 2`, data),
+  });
+}, 1000);
