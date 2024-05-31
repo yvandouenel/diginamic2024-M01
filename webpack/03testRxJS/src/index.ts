0406bb8071eb1a088abb75e37320c88cb321b32a
yvandouenel <y.douenel@coopernet.fr>
2024-05-31 14:24:16 +0200

    cold observable to hot.

--- webpack/03testRxJS/src/index.ts
+++ webpack/03testRxJS/src/index.ts
@@ -1,8 +1,9 @@
 import { Observable } from "rxjs";
+import { share, shareReplay } from "rxjs/operators";
 console.log(`HelloWorld`);
 
 // Création d'un observable froid
-const obs1$: Observable<number> = new Observable((observer) => {
+const obs1$ = new Observable((observer) => {
   // Notification next synchrone
   let i = 0;
   const intervalID = setInterval(() => {
@@ -13,18 +14,24 @@ const obs1$: Observable<number> = new Observable((observer) => {
       observer.complete();
     }
   }, 1000);
-});
+}).pipe(shareReplay(1));
 console.log(`obs1`, obs1$);
 
 // Souscription à l'observable
 const subscription1 = obs1$.subscribe({
-  next: (data) => console.log(`data dans observer 1`, data),
-  complete: () => console.log(`Oservable terminé`),
+  next: (data) => {
+    console.log(`data dans observer 1`, data);
+    if (data == 3) subscription1.unsubscribe();
+  },
+  complete: () => {
+    console.log(`Oservable terminé`);
+    subscription1.unsubscribe();
+  },
 });
 
-/* setTimeout(() => {
+setTimeout(() => {
   // Nouvelle ouscription à l'observable après 5 seconde
   const subscription2 = obs1$.subscribe({
     next: (data) => console.log(`data dans observer 2`, data),
   });
-}, 5000); */
+}, 5000);
